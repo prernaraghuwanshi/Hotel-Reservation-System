4bb3d28c677b31f88eceb97bc5060d3b4a3b9a99
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-05 22:28:02 +0530

    [Prerna]ADDED: Hotel class with attributes like name, rates and rating

--- src/main/java/com/bridgelabz/HotelReservation/Hotel.java
+++ src/main/java/com/bridgelabz/HotelReservation/Hotel.java
@@ -6,8 +6,42 @@ package com.bridgelabz.HotelReservation;
  */
 public class Hotel 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    private String hotelName;
+    private int weekdayRate;
+    private int weekendRate;
+    private int rating;
+	public String getHotelName() {
+		return hotelName;
+	}
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+	public int getWeekdayRate() {
+		return weekdayRate;
+	}
+	public void setWeekdayRate(int weekdayRate) {
+		this.weekdayRate = weekdayRate;
+	}
+	public int getWeekendRate() {
+		return weekendRate;
+	}
+	public void setWeekendRate(int weekendRate) {
+		this.weekendRate = weekendRate;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Hotel(String hotelName, int weekdayRate, int weekendRate, int rating) {
+		super();
+		this.hotelName = hotelName;
+		this.weekdayRate = weekdayRate;
+		this.weekendRate = weekendRate;
+		this.rating = rating;
+	}
+    
+    
+    
 }
--- src/main/java/com/bridgelabz/HotelReservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation/HotelReservation.java
@@ -2,7 +2,12 @@ package com.bridgelabz.HotelReservation;
 
 public class HotelReservation {
 	public static void main(String args[]) {
-		System.out.println("Welcom to Hotel Reservation Program!");
+		System.out.println("Welcome to Hotel Reservation Program!");
+		Hotel lakewood = new Hotel("Lakewood", 110, 90, 3);
+		Hotel bridgewood = new Hotel("Bridgewood", 160, 60, 4);
+		Hotel ridgewood = new Hotel("Ridgewood", 220, 150, 5);
+		
 	}
+	
 
 }
