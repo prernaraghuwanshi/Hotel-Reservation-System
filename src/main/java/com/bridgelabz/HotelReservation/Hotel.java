5b755ae593084b361531304826e3f23a0dfee340
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-13 12:27:40 +0530

    [Prerna]REFACTORED: Changed the dateToDay function from DateTimeFormatter to LocalDateTime. Formatted the code.

--- src/main/java/com/bridgelabz/HotelReservation/Hotel.java
+++ src/main/java/com/bridgelabz/HotelReservation/Hotel.java
@@ -1,39 +1,52 @@
 package com.bridgelabz.HotelReservation;
 
-/**
- * Hello world!
- *
- */
-public class Hotel 
-{
-    private String hotelName;
-    private int weekdayRate;
-    private int weekendRate;
-    private int rating;
+public class Hotel {
+	private String hotelName;
+	private int weekdayRate;
+	private int weekendRate;
+	private int rating;
+
+	// Getter for Hotel name
 	public String getHotelName() {
 		return hotelName;
 	}
+
+	// Setter for Hotel name
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
+
+	// Getter for Weekday rate
 	public int getWeekdayRate() {
 		return weekdayRate;
 	}
+
+	// Setter for Weekday rate
 	public void setWeekdayRate(int weekdayRate) {
 		this.weekdayRate = weekdayRate;
 	}
+
+	// Getter for Weekend rate
 	public int getWeekendRate() {
 		return weekendRate;
 	}
+
+	// Setter for Weekend rate
 	public void setWeekendRate(int weekendRate) {
 		this.weekendRate = weekendRate;
 	}
+
+	// Getter for Rating
 	public int getRating() {
 		return rating;
 	}
+
+	// Setter for Rating
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
+
+	// Constructor
 	public Hotel(String hotelName, int weekdayRate, int weekendRate, int rating) {
 		super();
 		this.hotelName = hotelName;
@@ -41,7 +54,4 @@ public class Hotel
 		this.weekendRate = weekendRate;
 		this.rating = rating;
 	}
-    
-    
-    
 }
