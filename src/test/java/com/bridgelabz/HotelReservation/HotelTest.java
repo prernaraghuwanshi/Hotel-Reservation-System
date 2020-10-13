5fbb76aa3156dcee0f32efa3f7adf5f8c2ae98ad
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-13 13:03:33 +0530

    [Prerna]REFACTORED: Formatted code and added descriptions of functions.

--- src/test/java/com/bridgelabz/HotelReservation/HotelTest.java
+++ src/test/java/com/bridgelabz/HotelReservation/HotelTest.java
@@ -1,78 +1,49 @@
 package com.bridgelabz.HotelReservation;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.text.ParseException;
 import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
 
-
-
-
-public class HotelTest 
-{
+public class HotelTest {
 	HotelReservation h = null;
-    @Before
-    public void initialize()
-    {
-    	h = new HotelReservation();
-    	
-    }
-    @Test
-    public void givenDateRange_WhenBothWeekday_shouldReturnLakewood()
-    {
-    	ArrayList<String> dateRange = new ArrayList<String>();
-    	dateRange.add("10/09/2020");
+
+	@Before
+	public void initialize() {
+		h = new HotelReservation();
+
+	}
+
+	@Test
+	public void givenDateRange_WhenBothWeekday_shouldReturnLakewood() {
+		ArrayList<String> dateRange = new ArrayList<String>();
+		dateRange.add("10/09/2020");
 		dateRange.add("11/09/2020");
-		try {
-			assertEquals("Lakewood", h.findCheapestBestRatedHotel(dateRange));
-			
-		} catch (ParseException e) {
-			
-		}
-    }
-    @Test
-    public void givenDateRange_WhenBothWeekend_shouldReturnLakewood()
-    {
-    	ArrayList<String> dateRange = new ArrayList<String>();
-    	dateRange.add("12/09/2020");
+		assertEquals("Lakewood", h.findCheapestBestRatedHotel(dateRange));
+	}
+
+	@Test
+	public void givenDateRange_WhenBothWeekend_shouldReturnBridgewood() {
+		ArrayList<String> dateRange = new ArrayList<String>();
+		dateRange.add("12/09/2020");
 		dateRange.add("13/09/2020");
-		try {
-			assertEquals("Bridgewood", h.findCheapestBestRatedHotel(dateRange));
-			
-		} catch (ParseException e) {
-			
-		}
-    }
-    @Test
-    public void givenDateRange_WhenOneWeekdayOneWeekend_shouldReturnBridgewood()
-    {
-    	ArrayList<String> dateRange = new ArrayList<String>();
-    	dateRange.add("11/09/2020");
+		assertEquals("Bridgewood", h.findCheapestBestRatedHotel(dateRange));
+	}
+
+	@Test
+	public void givenDateRange_WhenOneWeekdayOneWeekend_shouldReturnBridgewood() {
+		ArrayList<String> dateRange = new ArrayList<String>();
+		dateRange.add("11/09/2020");
 		dateRange.add("12/09/2020");
-		try {
-			assertEquals("Bridgewood", h.findCheapestBestRatedHotel(dateRange));
-			
-		} catch (ParseException e) {
-			
-		}
-    }
-    @Test
-    public void givenDateRange_WhenFindingBestRatedHotel_shouldReturnRidgewood()
-    {
-    	ArrayList<String> dateRange = new ArrayList<String>();
-    	dateRange.add("11/09/2020");
+		assertEquals("Bridgewood", h.findCheapestBestRatedHotel(dateRange));
+	}
+
+	@Test
+	public void givenDateRange_WhenFindingBestRatedHotel_shouldReturnRidgewood() {
+		ArrayList<String> dateRange = new ArrayList<String>();
+		dateRange.add("11/09/2020");
 		dateRange.add("12/09/2020");
-		try {
-			assertEquals("Ridgewood", h.findBestRatedHotel(dateRange));
-			
-		} catch (ParseException e) {
-			
-		}
-    }
-    
-    
+		assertEquals("Ridgewood", h.findBestRatedHotel(dateRange));
+	}
 }
