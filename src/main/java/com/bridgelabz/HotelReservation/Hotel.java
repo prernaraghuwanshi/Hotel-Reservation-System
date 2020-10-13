59a06953e8822810be09e69dd4e15613c63451db
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-10-13 13:11:37 +0530

    [Prerna]REFACTORED: Formatted code.

--- src/main/java/com/bridgelabz/HotelReservation/Hotel.java
+++ src/main/java/com/bridgelabz/HotelReservation/Hotel.java
@@ -1,62 +1,85 @@
 package com.bridgelabz.HotelReservation;
 
-/**
- * Hello world!
- *
- */
-public class Hotel 
-{
-    private String hotelName;
-    private int weekdayRate;
-    private int weekendRate;
-    private int rating;
-    private int weekdayRateReward;
-    private int weekendRateReward;
-    private boolean isRewardee;
-    
+public class Hotel {
+	private String hotelName;
+	private int weekdayRate;
+	private int weekendRate;
+	private int rating;
+	private int weekdayRateReward;
+	private int weekendRateReward;
+	private boolean isRewardee;
+
+	// Getter for hotel name
 	public String getHotelName() {
 		return hotelName;
 	}
+
+	// Setter for hotel name
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
+
+	// Getter for weekday rate for reward customer
 	public int getWeekdayRateReward() {
 		return weekdayRateReward;
 	}
+
+	// Setter for weekday rate for reward customer
 	public void setWeekdayRateReward(int weekdayRateReward) {
 		this.weekdayRateReward = weekdayRateReward;
 	}
+
+	// Getter for weekend rate for reward customer
 	public int getWeekendRateReward() {
 		return weekendRateReward;
 	}
+
+	// Setter for weekend rate for reward customer
 	public void setWeekendRateReward(int weekendRateReward) {
 		this.weekendRateReward = weekendRateReward;
 	}
-	public boolean isRewardee() {
+
+	// Getter for if a customer is a rewardee or not
+	public boolean getIsRewardee() {
 		return isRewardee;
 	}
+
+	// Setter for if a customer is a rewardee or not
 	public void setRewardee(boolean isRewardee) {
 		this.isRewardee = isRewardee;
 	}
+
+	// Getter for Weekday rate
 	public int getWeekdayRate() {
 		return weekdayRate;
 	}
+
+	// Setter for Weekday rate
 	public void setWeekdayRate(int weekdayRate) {
 		this.weekdayRate = weekdayRate;
 	}
+
+	// Getter for Weekend rate
 	public int getWeekendRate() {
 		return weekendRate;
 	}
+
+	// Setter for Weekend rate
 	public void setWeekendRate(int weekendRate) {
 		this.weekendRate = weekendRate;
 	}
+
+	// Getter for Rating
 	public int getRating() {
 		return rating;
 	}
+
+	// Setter for Rating
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-	
+
+	// Constructor
 	public Hotel(String hotelName, int weekdayRate, int weekendRate, int rating, int weekdayRateReward,
 			int weekendRateReward) {
 		super();
@@ -66,9 +89,5 @@ public class Hotel
 		this.rating = rating;
 		this.weekdayRateReward = weekdayRateReward;
 		this.weekendRateReward = weekendRateReward;
-		
 	}
-    
-    
-    
 }
--- src/main/java/com/bridgelabz/HotelReservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation/HotelReservation.java
@@ -1,159 +1,127 @@
 package com.bridgelabz.HotelReservation;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
-import java.util.List;
 
 public class HotelReservation {
 	Hotel lakewood = new Hotel("Lakewood", 110, 90, 3, 80, 80);
 	Hotel bridgewood = new Hotel("Bridgewood", 150, 50, 4, 110, 50);
 	Hotel ridgewood = new Hotel("Ridgewood", 220, 150, 5, 100, 40);
 	
-	public String dateToDay(String date) throws ParseException 
+	// Takes date and gives day of the week as output
+	public String dateToDay(String date) 
 	{
-		SimpleDateFormat format1=new SimpleDateFormat("dd/MM/yyyy");
-		Date dt = format1.parse(date);
-		DateFormat format2=new SimpleDateFormat("EEEE"); 
-		String day=format2.format(dt);
-		return day;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate localDate = LocalDate.parse(date, formatter);
+		return localDate.getDayOfWeek().name();
 	}
-	public int costOfHotel(Hotel hotelName, ArrayList<String> dateRange) throws ParseException
-	{
-		Iterator<String> it = dateRange.iterator();
-		int cost = 0;
-		while(it.hasNext())
-		{
-			String day = dateToDay(it.next());
-			
-			if(day.equals("Saturday") || day.equals("Sunday"))
-			{
-				cost += hotelName.getWeekendRate();
-			}
-			else
-			{
-				cost += hotelName.getWeekdayRate();
-			}
-		}
-		return cost;
-	}
-	public int minOfThree(int a, int b, int c)
-	{
-		return Math.min(Math.min(a, b), c);
-	}
-	public int maxOfThree(int a, int b, int c)
-	{
-		return Math.max(Math.max(a, b), c);
-	}
-	public String findCheapestHotel(ArrayList<String> dateRange) throws ParseException
-	{
-		int costOfLakewood = costOfHotel(lakewood, dateRange);
-		int costOfBridgewood = costOfHotel(bridgewood, dateRange);
-		int costOfRidgewood = costOfHotel(ridgewood, dateRange);
-		
-		int minCost = minOfThree(costOfLakewood, costOfBridgewood, costOfRidgewood);
-		
-		if(minCost == costOfLakewood)
-		{
-			System.out.println("Cheapest Hotel: Lakewood");
-			System.out.println("Cost = $"+ costOfLakewood);
-			return "Lakewood";
-		}
-		else if (minCost == costOfBridgewood)
-		{
-			System.out.println("Cheapest Hotel: Bridgewood");
-			System.out.println("Cost = $"+ costOfBridgewood);
-			return "Bridgewood";
-		}
-		else
-		{
-			System.out.println("Cheapest Hotel: Ridgewood");
-			System.out.println("Cost = $"+ costOfRidgewood);
-			return "Ridgewood";
-		}
-	}
-	public String findCheapestBestRatedHotel(ArrayList<String> dateRange) throws ParseException
-	{
-		int costOfLakewood = costOfHotel(lakewood, dateRange);
-		int costOfBridgewood = costOfHotel(bridgewood, dateRange);
-		int costOfRidgewood = costOfHotel(ridgewood, dateRange);
-		int index = -1;
-		int minCost = minOfThree(costOfLakewood, costOfBridgewood, costOfRidgewood);
-		List<Hotel> hotelList = new ArrayList<Hotel>();
-		if(minCost == costOfLakewood)
-		{
-			hotelList.add(lakewood);
-		}
-		if (minCost == costOfBridgewood)
-		{
-			hotelList.add(bridgewood);
-		}
-		if (minCost == costOfRidgewood)
-		{
-			hotelList.add(ridgewood);
-		}
-		if(hotelList.size() == 1)
-		{
-			index = 0;
-		}
-		else
-		{
-			int maxRating = 0;
-			for(int i = 0; i< hotelList.size(); i++)
-			{
-				if(hotelList.get(i).getRating() > maxRating)
-				{
-					maxRating = hotelList.get(i).getRating();
-					index = i;
+	// Function to find cost of a hotel
+		public int costOfHotel(Hotel hotelName, ArrayList<String> dateRange) {
+			Iterator<String> it = dateRange.iterator();
+			int cost = 0;
+			while (it.hasNext()) {
+				String day = dateToDay(it.next());
+				if (day == "SATURDAY" || day == "SUNDAY") {
+					cost += hotelName.getWeekendRate();
+				} else {
+					cost += hotelName.getWeekdayRate();
 				}
 			}
+			return cost;
 		}
-		System.out.println("Cheapest Hotel: "+hotelList.get(index).getHotelName());
-		System.out.println("Cost = $"+ minCost);
-		System.out.println("Rating: "+hotelList.get(index).getRating());
-		return hotelList.get(index).getHotelName();
-	}
-	public String findBestRatedHotel(ArrayList<String> dateRange) throws ParseException
-	{
-		int bestRating = maxOfThree(lakewood.getRating(), bridgewood.getRating(), ridgewood.getRating());
-		String outputHotel;
-		int cost; 
-		if(bestRating == lakewood.getRating())
-		{
-			outputHotel = "Lakewood";
-			cost = costOfHotel(lakewood, dateRange);
-		}
-		else if (bestRating == bridgewood.getRating())
-		{
-			outputHotel = "Bridgewood";
-			cost = costOfHotel(bridgewood, dateRange);
-		}
-		else
-		{
-			outputHotel = "Ridgewood";
-			cost = costOfHotel(ridgewood, dateRange);
-		}
-		System.out.println("Best rated Hotel: "+outputHotel);
-		System.out.println("Cost = $"+ cost);
-		System.out.println("Rating: "+ bestRating);
-		return outputHotel;
-	}
-	public static void main(String args[]) throws ParseException {
-		System.out.println("Welcome to Hotel Reservation Program!");
-//		HotelReservation h = new HotelReservation();
-//
-//		ArrayList<String> dateRange = new ArrayList<String>();
-//		dateRange.add("11/09/2020");
-//		dateRange.add("12/09/2020");
-//
-//		h.findCheapestHotel(dateRange);
-//		h.findCheapestBestRatedHotel(dateRange);
-//		h.findBestRatedHotel(dateRange);
-		
-	}
-	
 
+		// Function to find minimum of three value
+		public int minOfThree(int value1, int value2, int value3) {
+			return Math.min(Math.min(value1, value2), value3);
+		}
+
+		// Function to find Cheapest hotel in the given date range
+		public String findCheapestHotel(ArrayList<String> dateRange) {
+			int costOfLakewood = costOfHotel(lakewood, dateRange);
+			int costOfBridgewood = costOfHotel(bridgewood, dateRange);
+			int costOfRidgewood = costOfHotel(ridgewood, dateRange);
+			int minCost = minOfThree(costOfLakewood, costOfBridgewood, costOfRidgewood);
+			if (minCost == costOfLakewood) {
+				System.out.println("Cheapest Hotel: Lakewood");
+				System.out.println("Cost = $" + costOfLakewood);
+				return "Lakewood";
+			} else if (minCost == costOfBridgewood) {
+				System.out.println("Cheapest Hotel: Bridgewood");
+				System.out.println("Cost = $" + costOfBridgewood);
+				return "Bridgewood";
+			} else {
+				System.out.println("Cheapest Hotel: Ridgewood");
+				System.out.println("Cost = $" + costOfRidgewood);
+				return "Ridgewood";
+			}
+		}
+
+		// Function to find maximum of three values
+		public int maxOfThree(int value1, int value2, int value3) {
+			return Math.max(Math.max(value1, value2), value3);
+		}
+
+		// Function to find cheapest and best rated hotel
+		public String findCheapestBestRatedHotel(ArrayList<String> dateRange) {
+			int costOfLakewood = costOfHotel(lakewood, dateRange);
+			int costOfBridgewood = costOfHotel(bridgewood, dateRange);
+			int costOfRidgewood = costOfHotel(ridgewood, dateRange);
+			int minCost = minOfThree(costOfLakewood, costOfBridgewood, costOfRidgewood);
+			ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+			if (minCost == costOfLakewood) {
+				hotelList.add(lakewood);
+			}
+			if (minCost == costOfBridgewood) {
+				hotelList.add(bridgewood);
+			}
+			if (minCost == costOfRidgewood) {
+				hotelList.add(ridgewood);
+			}
+			if (hotelList.size() == 1) {
+				System.out.println("Cheapest Hotel: " + hotelList.get(0).getHotelName());
+				System.out.println("Cost = $" + minCost);
+				System.out.println("Rating: " + hotelList.get(0).getRating());
+				return hotelList.get(0).getHotelName();
+			} else {
+				int maxRating = 0;
+				int index = -1;
+				for (int i = 0; i < hotelList.size(); i++) {
+					if (hotelList.get(i).getRating() > maxRating) {
+						maxRating = hotelList.get(i).getRating();
+						index = i;
+					}
+				}
+				System.out.println("Cheapest Hotel: " + hotelList.get(index).getHotelName());
+				System.out.println("Cost = $" + minCost);
+				System.out.println("Rating: " + hotelList.get(index).getRating());
+				return hotelList.get(index).getHotelName();
+			}
+		}
+
+		// Function to find best rated hotel
+		public String findBestRatedHotel(ArrayList<String> dateRange) {
+			int bestRating = maxOfThree(lakewood.getRating(), bridgewood.getRating(), ridgewood.getRating());
+			String outputHotel;
+			int cost;
+			if (bestRating == lakewood.getRating()) {
+				outputHotel = "Lakewood";
+				cost = costOfHotel(lakewood, dateRange);
+			} else if (bestRating == bridgewood.getRating()) {
+				outputHotel = "Bridgewood";
+				cost = costOfHotel(bridgewood, dateRange);
+			} else {
+				outputHotel = "Ridgewood";
+				cost = costOfHotel(ridgewood, dateRange);
+			}
+			System.out.println("Best rated Hotel: " + outputHotel);
+			System.out.println("Cost = $" + cost);
+			System.out.println("Rating: " + bestRating);
+			return outputHotel;
+		}
+
+		public static void main(String args[]) {
+			System.out.println("Welcome to Hotel Reservation Program!");
+		}
 }
